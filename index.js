2e114c3eb03046596ac029eb6c80cbd72ab2db45
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-05-11 11:26:07 +0530

    update

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ const sitemapRoute = require("./routes/sitemap");
 const employeeRoutes = require("./routes/employee.routes.js");
 const app = express();
 
-app.use(express.json({
+app.use(express.json());
+app.use(cors({
   origin: "*",
 }));
-app.use(cors());
 app.use("/uploads", express.static("uploads"));
 
 // Routes
